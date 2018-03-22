ac6000a845c2f7c255fc776e575eb3f0547682ad
Frisoni <frisoni@gmail.com>
2018-03-22 11:52:42 -0300

    Update html-calculator-fields.php

--- templates/frontend/site/html-calculator-fields.php
+++ templates/frontend/site/html-calculator-fields.php
@@ -1,6 +1,6 @@
 <div id='shipping-calc'>
 	<div id='wscp-form'>
-	<input type='tel' id='wscp-postcode' autocomplete="postal-code" placeholder="<?=get_option('wscip_placeholder','Insira o seu CEP')?>" name='wscp-postcode' class='input-text fl-input' />
+	<input type='tel' id='wscp-postcode' autocomplete="postal-code" placeholder="<?=get_option('wscip_placeholder','Insira o seu CEP')?>" name='wscp-postcode' class='input-text text' />
 	<input type='button' id='wscp-button' class='button wscp-button' value='Calcular' style="color: <?=get_option('wscip_btn_color_text','#ffffff');?>; background-color: <?=get_option('wscip_btn_color','#999999');?>">
 	<input type='hidden' name='wscp-nonce' id='wscp-nonce' value='<?= wp_create_nonce( 'wscp-nonce' ); ?>'>
 	</div>
